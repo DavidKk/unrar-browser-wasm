b07164aceb8c551fadf2ccf9ec83c0816c3d8dc1
DavidKk <DavidKk@users.noreply.github.com>
2025-11-12 18:56:59 +0000

    deploy: d332bbf1529926569a6186559408daa1450b190d

---
+++ CMakeFiles/4.1.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-24.6.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "24.6.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/runner/work/_temp/cca666fa-fd28-4ff9-a69f-41017d27c454/emsdk-main/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+
+set(CMAKE_SYSTEM "Emscripten-1")
+set(CMAKE_SYSTEM_NAME "Emscripten")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "x86")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ CMakeFiles/unrar.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for unrar.
+# This may be replaced when dependencies are built.
